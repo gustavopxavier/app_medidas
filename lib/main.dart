714587d947e9f8db9e69453286174e126bfa9e86
gustavopxavier <gustavopxavier@gmail.com>
2021-05-18 21:23:00 -0300

    TODO splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       title: 'Aplicativo Medidas',
+      //TODO: Fazer splash screen
       home: AnimatedSplashScreen(
         duration: 3000,
         splash: Icons.home,
